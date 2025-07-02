9237ac50edc1e4ae75edb4cfa66524a3910c2c99
felipeMcavalcante <felipe.de.moraes.cavalcante@gmail.com>
2025-07-02 16:05:40 -0300

    teste commit correto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
-import Insta from '@/components/Insta';
+// import Insta from '@/components/Insta';
 import Projects from '@/components/Projects';
 import Santos from '@/components/Santos';
 import Serie from '@/components/Serie';
@@ -17,7 +17,7 @@ export default function Home() {
       <Santos />
       <Serie />
       <Projects />
-      <Insta />
+      {/* <Insta /> */}
       <Contact />
       <Footer />
     </main>
